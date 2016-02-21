58f6b3012cd5da84c7abbee1fc1b236bfdd1f4d4
Michael Bishop <mbtyke@gmail.com>
2016-02-21 08:05:43 -0500

    Added a token for logged in users that the socket uses to authenticate connections.

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -11,7 +11,7 @@ defmodule Rumbl.Auth do
       # pass through current user if it exists. This allows
       # us to add logged in users from the testing harness
       user = conn.assigns[:current_user] ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
       user = user_id && repo.get(Rumbl.User, user_id) ->
         assign(conn, :current_user, user)
       true ->
@@ -21,9 +21,16 @@ defmodule Rumbl.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def logout(conn) do
